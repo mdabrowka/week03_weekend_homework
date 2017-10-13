9ea82b258bbdd776d6fc7a757fb38743c4506ef6
Marta <martadabrowka@gmail.com>
2017-10-13 11:04:40 +0100

    fixed database problems

--- db/cinema.sql
+++ db/cinema.sql
@@ -5,16 +5,18 @@ DROP TABLE films;
 
 CREATE TABLE customers (
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(255),
+  funds INT4
 );
 
 CREATE TABLE films (
   id SERIAL4 PRIMARY KEY,
-  title VARCHAR(255)
+  title VARCHAR(255),
+  price INT4
 );
 
 CREATE TABLE tickets (
   id SERIAL4 PRIMARY KEY,
-  customer_id INT4 REFERENCES customers.id,
-  film_id INT4 REFERENCES titles.id
+  customer_id INT4 REFERENCES customers(id),
+  film_id INT4 REFERENCES films(id)
 );
