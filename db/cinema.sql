3ad717032a5583d04bacf70d18fc4113a3cdc264
Marta <martadabrowka@gmail.com>
2017-10-13 10:40:57 +0100

    set up database structure and folders

---
+++ db/cinema.sql
@@ -0,0 +1,20 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255)
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers.id,
+  film_id INT4 REFERENCES titles.id
+);
